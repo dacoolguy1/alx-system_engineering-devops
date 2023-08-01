8f175102bee176025eb397f3ce385638cce9c5a0
Emmanuel Temiede <68757523+dacoolguy1@users.noreply.github.com>
2023-08-01 23:58:34 +0100

    Add files via upload

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""A Flask app that has a single route
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+class Config():
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# lets instantiate the class
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
